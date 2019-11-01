730b3047d58cb66a4de581692a06d392c5bbce56
McLaren College <53505683+McLarenCollege@users.noreply.github.com>
2019-11-01 08:50:24 +0530

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -31,15 +31,15 @@
 // Create a function that groups an array of numbers based on a size parameter. The size represents the maximum length of each sub-array.
 //
 //  [1, 2, 3, 4, 5, 6], 3
-//  [[1, 3, 5], [2, 4, 6]]
+//  [[1, 2, 3], [4, 5, 6]]
 /// Divide array into groups of size 3.
 //
 //  [1, 2, 3, 4, 5, 6], 2
-//  [[1, 4], [2, 5], [3, 6]]
+//  [[1, 2], [3, 4], [5, 6]]
 /// Divide array into groups of size 2.
 //
 //  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 4
-//  [[1, 4, 7, 10], [2, 5, 8, 11], [3, 6, 9]]
+//  [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11]]
 /// "Leftover" arrays are okay.
 
 main() {
